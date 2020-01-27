3fdf1ac7c5123ac10a4b92be6b0d63a3377c43ef
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-27 18:28:22 -0500

    completes section 1 practice

--- 1.php
+++ 1.php
@@ -15,20 +15,17 @@
 		
 
 
-		<?php  
+<?php  
+    // Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
+    // Step 2: Write a comment above the echo function and explain what that function is doing.
 
 
-		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
+  // This function displays the word 'Hello' using h1 tag
+  echo "<h1>Hello</h1>";
+
+?>
 
 
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
-
-
-		   */
-
-		?>
 
 	
 
