6954005360a2c02b4f75043e88120dbb06416240
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-30 21:06:13 -0500

    completes section 3

--- 3.php
+++ 3.php
@@ -14,16 +14,56 @@
 
 <?php  
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+//  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+
+if (4 > 10) {
+  echo "I love Ruby";
+} elseif (10 < 4) {
+  echo "I love Java";
+} else echo "I love PHP";
 
 
 
-	Step 2: Make a forloop  that displays 10 numbers
 
 
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
+//Step 2: Make a forloop  that displays 10 numbers
 
- */
+for ($i=0; $i <= 10; $i++) { 
+  echo $i . "<br>";
+}
+
+
+
+
+
+//Step 3 : Make a switch Statement that test againts one condition with 5 cases
+
+$num = 10;
+
+switch ($num) {
+  case 1:
+    echo "the num is 1";
+    break;
+  case 100:
+    echo "the num is 100";
+    break;
+  case 5:
+    echo "the num is 5";
+    break;
+  case 4:
+    echo "the num is 4";
+    break;
+  case 10:
+    echo "the num is 10";
+    break;
+  
+  default:
+    "cant find your num";
+    break;
+}
+
+
+
 
 	
 ?>
