e5e897f4c0daae387ef3ab036b73525b1af4d23b
crabtree <justin.kyle.crabtree@gmail.com>
2020-02-05 18:54:31 -0500

    completes section 5

--- 5.php
+++ 5.php
@@ -12,18 +12,33 @@
 <article class="main-content col-xs-8">
 
 	
-	<?php 
+<?php 
 
 
-/*  Step1: Use a pre-built math function here and echo it
+// Step1: Use a pre-built math function here and echo it
+
+echo pow(5, 5);
+echo "<br>";
 
 
-	Step 2:  Use a pre-built string function here and echo it
 
 
-	Step 3:  Use a pre-built Array function here and echo it
+// Step 2:  Use a pre-built string function here and echo it
 
- */
+$string = "Ora Ora Ora!";
+echo strtoupper($string);
+echo "<br>";
+
+
+
+
+// Step 3:  Use a pre-built Array function here and echo it
+
+$array = [5, 15, 89, 1023, 2, 456456, 678];
+echo max($array);
+
+
+
 
 	
 ?>
