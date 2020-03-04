c79dde8497954974d81c7f26db585460b8f303eb
crabtree <justin.kyle.crabtree@gmail.com>
2020-03-03 19:07:48 -0500

    completes section 9

--- 9.php
+++ 9.php
@@ -19,15 +19,29 @@
 	
 	<?php 
 
-	/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
+	// Create a link saying Click Here, and set the link href to pass some parameters and use the GET super global to see it
 
-		Step 2 - Set a cookie that expires in one week
+	//	Step 2 - Set a cookie that expires in one week
 
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
-	
+	//	Step 3 - Start a session and set it to value, any value you want.
+
+
+  print_r($_GET);
+  $id = 1;
+  $button = "Click Here"
+
+  $name = "Justin";
+  $value = 3;
+  $expiration = time() + (60 * 60 * 24 * 7);
+  setcookie($name, $value, $expiration);
+
+  session_start();
+  $_SESSION['message'] = "Testing Message";
+
 	?>
+
+
+<a href="9.php?id=<?php echo $id;?>"><?php echo $button?></a>
 
 
 
